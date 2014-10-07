85e83c12745e7b296b8ae53a3e3ea17e7b2004d4
Mixbo <1234675@408W21.decinfo.cvm>
2014-10-07 14:09:18 -0400

    Minimap offsets

--- src/Controller.py
+++ src/Controller.py
@@ -74,10 +74,30 @@ class EventListener:
             self.controller.view.selection()
             self.controller.view.update(self.controller.model.units, None)
 
+        # Minimap
         if event.x >= self.controller.view.width - 233 and event.x <= self.controller.view.width - 22:
             if event.y >= 18 and event.y <= 229:
-                self.controller.view.positionX = int((event.x - self.controller.view.width-233)/2)+233
-                self.controller.view.positionY = int((event.y - 18) /2)
+                posClicX = int((event.x - self.controller.view.width-233)/2)+233
+                posClicY = int((event.y - 18) /2)
+
+                posRealX = posClicX - 8
+                posRealY = posClicY - 7
+
+                if posRealX < 0:
+                	posRealX = 0
+
+                elif posRealX > 89:
+                	posRealX = 89
+
+                if posRealY < 0:
+                	posRealY = 0
+
+                elif posRealY > 91:
+                	posRealY = 91
+
+
+                self.controller.view.positionX = posRealX
+                self.controller.view.positionY = posRealY
                 #self.controller.view.drawMinimap(self.controller.model.units,self.controller.model.carte.matrice)
                 self.controller.view.update(self.controller.model.units, self.controller.model.carte.matrice)
 
