6718e410a968f95d46c1a5afe7d7b527b7c2e8b4
patenteapoil <liam.giverin@gmail.com>
2014-10-28 15:48:11 -0400

    ajout de la recherche de la ressource la plus proche

--- src/AI.py
+++ src/AI.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import time
-import Model
+from Batiments import *
+
 
 #TODO mettre les bonnes valeurs pour les couts de construction et de recherche
+from Units import *
+
 
 class AI:
     def __init__(self,parent, civilisation):
         self.civilisation = civilisation
+        self.base = Base(self, 10, 10)
         self.parent = parent
         self.manqueRessource = False
         self.ressourceManquante = None
@@ -17,11 +20,12 @@ class AI:
         self.bois = 0
         self.minerais = 0
         self.charbon = 0
-        self.nourriture = 0
+        self.nbNourriture = 0
         self.units = []
+        parent.createUnit(Unit.generateId(self.civilisation),self.base.posX+1, self.base.posY+1, self.civilisation)
         self.paix = True
         self.paysansOccupes = True
-        self.moral = 100
+        self.morale = 100
         self.nombreSoldatsAllies = 0
         self.nombreSoldatsEnnemis = 0
         self.hpMoyen = 0
@@ -42,10 +46,16 @@ class AI:
         self.cooldownAttaque = 60
         self.derniereAttaque = 0
         self.cooldownRecherche = 120
+        self.cooldownRessource = 30
         self.dernierSoldat = 0
         self.dernierPaysan = 0
         self.cooldownUnite = 40
         self.derniereRecherche = 0
+        self.derniereRessourceBois = 0
+        self.derniereRessourceMinerai = 0
+        self.derniereRessourceCharbon = 0
+        self.derniereBase = 0
+        self.cooldownBatiment = 60
 
     def penser(self):
 
@@ -67,7 +77,7 @@ class AI:
                             self.manqueRessource = False
                             pass
                     elif self.ressourceManquante == "nourriture":
-                        if self.qteRessourceManquante <= self.nourriture:
+                        if self.qteRessourceManquante <= self.nbNourriture:
                             self.manqueRessource = False
                             pass
                     elif self.ressourceManquante == "charbon":
@@ -90,7 +100,7 @@ class AI:
                 return
 
             #si le moral est trop bas, faire une fête
-            if self.moral < 50 and time.time() - self.derniereFete >= self.cooldownRecherche:
+            if self.morale < 50 and time.time() - self.derniereFete >= self.cooldownRecherche:
                 self.augmenterMoral()
                 self.derniereFete = time.time()
                 return
@@ -119,6 +129,7 @@ class AI:
 
 
     def chercherRessource(self, ressource):
+        print(ressource)
         if ressource == "bois":
             self.chercherBois()
         elif ressource == "minerais":
@@ -136,8 +147,13 @@ class AI:
                         if time.time() - self.derniereScierie >= self.cooldownAutomatisation:
                             #TODO creer une scierie et y mettre le paysan
                             return
-                    #TODO faire chercher la ressource
-                    return
+
+                    if time.time() - self.derniereRessourceBois >= self.cooldownRessource:
+                        position = self.trouverRessourcePlusPres(unit, self.parent.parent.carte.FORET)
+                        unit.changerCible(position["x"], position["y"])
+                        self.derniereRessourceBois = time.time()
+                        return
+
         self.creerPaysan()
 
     def chercherminerais(self):
@@ -148,16 +164,25 @@ class AI:
                         if time.time() - self.derniereFonderie >= self.cooldownAutomatisation:
                             #TODO creer une fonderie et y mettre le paysan
                             return
-                    #TODO faire chercher la ressource
-                    return
+                    if time.time() - self.derniereRessourceMinerai >= self.cooldownRessource:
+                        position = self.trouverRessourcePlusPres(unit, self.parent.parent.carte.MINERAI)
+                        unit.changerCible(position["x"], position["y"])
+                        self.derniereRessourceBois = time.time()
+                        return
         self.creerPaysan()
 
     def cherchercharbon(self):
         for unit in self.units:
             if isinstance(unit, Paysan):
                 if unit.typeRessource == 0:
-                    #TODO faire chercher la ressource
-                    return
+                    if time.time() - self.derniereRessourceCharbon >= self.cooldownRessource:
+                        for x in range(unit.x, unit.x+20):
+                            for y in range(unit.y, unit.y+20):
+                                if time.time() - self.derniereRessourceBois >= self.cooldownRessource:
+                                    position = self.trouverRessourcePlusPres(unit, self.parent.parent.carte.CHARBON)
+                                    unit.changerCible(position["x"], position["y"])
+                                    self.derniereRessourceBois = time.time()
+                                    return
         self.creerPaysan()
 
     def chercherNourriture(self):
@@ -165,12 +190,13 @@ class AI:
             if isinstance(unit, Paysan):
                 if unit.typeRessource == 0:
                         if time.time() - self.derniereFerme >= self.cooldownAutomatisation:
-                            #TODO creer une scierie
+                            #TODO creer une ferme
                             return
 
         self.creerPaysan()
 
     def changerEpoque(self):
+        print("changer Epoque")
         for batiment in self.batiments:
             if isinstance(batiment, Base):
                 if self.epoque == 1:
@@ -199,20 +225,30 @@ class AI:
         #TODO trouver un paysan et le faire créer une nouvelle base
 
     def creerPaysan(self):
-        for batiment in self.batiments:
-            if isinstance(batiment, Base):
-                if self.bois >= batiment.coutCreer1[0]:
-                    batiment.creer1()
+        print("creer Paysan")
+        if self.base != None:
+                if self.bois >= self.base.coutCreer1[0]:
+                    self.base.creer1()
                     return
                 else:
                     self.manqueRessource = True
-                    self.qteRessourceManquante = batiment.coutCreer1[0]
+                    self.qteRessourceManquante = self.base.coutCreer1[0]
                     self.ressourceManquante = "bois"
                     return
 
         #TODO trouver paysan et créer une nouvelle base
+        if time.time() - self.derniereBase >= self.cooldownBatiment:
+            for unit in self.units:
+                if isinstance(unit, Paysan):
+                    if unit.typeRessource == 0:
+                        position = self.trouverRessourcePlusPres(unit, self.parent.parent.carte.Gazon)
+                        self.base = Base(self, position["x"], position["y"])
+
+                        
+
 
     def augmenterMoral(self):
+        print("augmenter Moral")
         for batiment in self.batiments:
             if isinstance(batiment, Eglise):
                 if self.bois >= batiment.coutRecherche1[0]:
@@ -226,6 +262,7 @@ class AI:
         #TODO trouver un paysan et creer une nouvelle Église
 
     def creerSoldat(self):
+        print("creer Soldat")
         for batiment in self.batiments:
             if isinstance(batiment, Baraque):
                 if not batiment.enCreation:
@@ -240,18 +277,21 @@ class AI:
         #TODO trouver un paysan et creer une nouvelle baraque
 
     def attaquer(self):
+        print("attaquer")
         for unit in self.units:
             if isinstance(unit, Soldat):
                 #TODO faire Attaquer les Soldats
                 pass
 
     def retraite(self):
+        print("retraite")
         for unit in self.units:
             if isinstance(unit, Soldat):
                 #TODO faire retraiter les Soldats
                 pass
 
     def rechercher(self):
+        print("rechercher")
         #TODO figurer comment rechercher
         pass
 
@@ -302,4 +342,40 @@ class AI:
                 if self.nombreSoldatsEnnemis < i:
                     self.nombreSoldatsEnnemis = i
 
+    def trouverRessourcePlusPres(self, unit, typeRessource):
+        ressource = {"x" : 0 , "y" : 0}
+
+        minX = unit.x - 5
+        minY = unit.y - 5
+        maxX = unit.x + 5
+        maxY = unit.y + 5
+        found = False
+
+        while not found:
+            if minX < self.parent.parent.carte.size:
+                minX = 0
+
+            if minY < self.parent.parent.carte.size:
+                minY = 0
+
+            if maxX > self.parent.parent.carte.size:
+                minX = self.parent.parent.carte.size
+
+            if maxY > self.parent.parent.carte.size:
+                maxY = self.parent.parent.carte.size
+
+            for x in range (minX, maxX):
+                for y in range (minY, maxY):
+                    if self.parent.parent.carte.matrice[x][y].type == typeRessource:
+                        found = True
+
+            minX -= 5
+            minY -= 5
+            maxX += 5
+            maxY += 5
+
+        ressource["x"] = x
+        ressource["y"] = y
+        print(str(x) + " " + str(y))
+        return ressource
 
